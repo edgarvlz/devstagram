917fddd4af4db28622aabc956e67403d42012bfa
Edgar Valenzuela <edgarjaed@gmail.com>
2024-05-13 21:24:30 -0400

    104

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -4,79 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Imagick\Driver;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+    //
     public function store(Request $request)
     {
         $imagen = $request->file('file');
 
-        $nombreImagen = Str::uuid() . '.' . $imagen->extension();
+        $nombreImagen = Str::uuid().".".$imagen->extension();
 
-        //instanciar Intervention\Image
-        $manager = new ImageManager(new Driver());
-        //leer la imagen
-        $imagenServidor = $manager::imagick()->read($imagen);
-        //redimencionar la imagen
-        $imagenServidor->resizeDown(1000, 1000);
-        //crear la ruta de la imagen
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000);
+
         $imagenPath = public_path('uploads') . '/' . $nombreImagen;
-        //guardar la imagen en la ruta especificada
         $imagenServidor->save($imagenPath);
 
-        return response()->json($nombreImagen);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,13 +11,13 @@
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-6/12 px-10">
-            <form id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center" action="/imagenes">
+            <form id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center" action="/imagenes" enctype="multipart/form-data">
                 @csrf
             </form>
         </div>
 
         <div class="md:w-6/12 p-6 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
-            <form action="{{ route('imagenes.store') }}" method="post" enctype="multipart/form-data" novalidate>
+            <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data" novalidate>
                 @csrf
                 <div class="mb-5">
                     <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
